d088c211d6a5dc1cace4b823cd804a8d93b7dcf6
Nick Field <heyimnick@live.com>
2017-06-12 11:20:53 +0100

    add client side handlers for messaging

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,6 +4,9 @@ import {Socket} from "phoenix"
 const user_token = window.user_token;
 const user_id = window.user_id;
 
+// Store pathname if remote 
+const remote_path = window.location.pathname.slice(1) || false;
+
 // Append to DOM
 const output = () => {
   document.querySelector("#output")
@@ -35,13 +38,73 @@ socket.connect();
 const channel = socket.channel(`user:${model.user_id}`, {})
 channel.join()
   .receive("ok", resp => {
-    // Set window url
-    window.history.replaceState('', '', '/' + model.user_id)
-    // Update state
-    model.state = 'READY FOR REMOTE';
-    // Display output
-    output();
-    // Handle error
+    // If remote
+    if (remote_path) {
+      // Set remote id
+      model.remote_id = remote_path;
+      // Update state
+      model.state = 'MESSAGING REMOTE';
+      // Push msg
+      channel.push("msg", {name: model.name, remote_id: model.remote_id})   
+      // Display output
+      output();
+    } else {
+      // Update url
+      window.history.replaceState('', '', '/' + model.user_id)
+      // Update state
+      model.state = 'READY FOR REMOTE';
+      // Display output
+      output();
+      }
+    // On error
     }).receive("error", resp => { console.log("Unable to join", resp) })
+
+// Handle connect (from remote) 
+channel.on("msg:connect", msg => {
+  // Alert User
+  window.alert(msg.name + ' would like to connect. Press ok to conect')
+  // Set remote_id
+  model.remote_id = msg.remote_id;
+  // Send accept
+  channel.push("msg", {name: model.name, remote_id: model.remote_id })
+  // Set state
+  model.state = "IN CHAT";
+  // Output
+  output();
+})
+
+// Handle accept (from user) 
+channel.on("msg:accept", msg => {
+  // Alert Remote 
+  window.alert(msg.name + ' has accepted')
+  // Set remote name
+  model.remote_name = msg.remote_name;
+  // Send new msg
+  channel.push("msg", {body: "hello"})
+  // Set state
+  model.state = "IN CHAT";
+  // Output
+  output();
+})
+
+// Handle denied (from user)
+channel.on("msg:denied", msg => {
+  // Alert Remote
+  window.alert(msg.name + ' has denied')
+  // Reset window url
+  window.history.replaceState('', '', '/')
+  // Reset remote id
+  model.remote_id = "";
+  // Reset state
+  model.state = "IDLE";
+  // Output
+  output();
+})
+
+// Handle new messages 
+channel.on("msg:new", msg => {
+  // Simply alert user of message
+  window.alert(msg.body)
+})
 
 export default socket
